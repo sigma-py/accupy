7ce03d53886977a819e2c1ed643d1901eb4ef263
Nico Schlömer <nico.schloemer@gmail.com>
2018-02-26 17:50:32 +0100

    explicitly disallow the copy in distill()

--- src/pybind11.cpp
+++ src/pybind11.cpp
@@ -3,11 +3,12 @@
 
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 PYBIND11_MODULE(_accupy, m) {
   m.def("distill", &distill);
   m.def("kahan", &kahan);
   m.def("neumaier", &neumaier);
   m.def("kdot_helper", &kdot_helper);
-  m.def("distill", &distill);
+  m.def("distill", &distill, "r"_a.noconvert());
 }
